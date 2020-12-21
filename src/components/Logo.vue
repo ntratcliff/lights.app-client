3b8924a0273168c76b0afa24606847dae4eb9554
Noah Ratcliff <me@nratcliff.com>
2020-12-21 14:47:02 -0500

    feat: clean up navbar

---
+++ src/components/Logo.vue
@@ -0,0 +1,3 @@
+<template>
+	<span id="logo">&#x1F4A1;.app</span>
+</template>
--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -4,7 +4,7 @@
 		type="dark"
 		variant="info"
 	>
-		<b-navbar-brand href="#">Bootstrap-Vue Webpack Boilerplate</b-navbar-brand>
+		<b-navbar-brand href="#"><logo /></b-navbar-brand>
 		<b-navbar-toggle target="nav-collapse" />
 
 		<b-collapse
@@ -12,51 +12,18 @@
 			is-nav
 		>
 			<b-navbar-nav>
-				<b-nav-item href="#">Link</b-nav-item>
-				<b-nav-item
-					href="#"
-					disabled
-				>
-					Disabled
-				</b-nav-item>
-			</b-navbar-nav>
-
-			<!-- Right aligned nav items -->
-			<b-navbar-nav class="ml-auto">
-				<b-nav-form>
-					<b-form-input
-						size="sm"
-						class="mr-sm-2"
-						placeholder="Search"
-					/>
-					<b-button
-						size="sm"
-						class="my-2 my-sm-0"
-						type="submit"
-					>
-						Search
-					</b-button>
-				</b-nav-form>
-
-				<b-nav-item-dropdown
-					text="Lang"
-					right
-				>
-					<b-dropdown-item href="#">EN</b-dropdown-item>
-					<b-dropdown-item href="#">ES</b-dropdown-item>
-					<b-dropdown-item href="#">RU</b-dropdown-item>
-					<b-dropdown-item href="#">FA</b-dropdown-item>
-				</b-nav-item-dropdown>
-
-				<b-nav-item-dropdown right>
-					<!-- Using 'button-content' slot -->
-					<template slot="button-content">
-						<em>User</em>
-					</template>
-					<b-dropdown-item href="#">Profile</b-dropdown-item>
-					<b-dropdown-item href="#">Sign Out</b-dropdown-item>
-				</b-nav-item-dropdown>
+				<b-nav-item to="/">Home</b-nav-item>
 			</b-navbar-nav>
 		</b-collapse>
 	</b-navbar>
 </template>
+
+<script>
+import Logo from 'components/Logo'
+
+export default {
+	components: {
+		Logo
+	}
+}
+</script>
