55c35451bb0a6326582f724a81a4955077138ce3
Noah Ratcliff <me@nratcliff.com>
2020-12-21 16:33:10 -0500

    feat: view/set light value

---
+++ src/components/LightControl.vue
@@ -0,0 +1,84 @@
+<template>
+	<div>
+		<h3>{{ name }}</h3>
+		<label for="brightness">Brightness</label>
+		<b-form-input
+			id="brightness"
+			v-model="brightness"
+			type="range"
+			min="0"
+			max="255"
+		/>
+	</div>
+</template>
+
+<script>
+import axios from 'axios'
+import _ from 'lodash'
+
+const apiURL = 'http://192.168.1.120:8081'
+
+export default {
+	props: {
+		name: {
+			type: String,
+			required: true
+		},
+		lightId: {
+			type: Number,
+			required: true
+		},
+		debounceRate: {
+			type: Number,
+			default: 50
+		},
+		updateRate: {
+			type: Number,
+			default: 1000
+		}
+	},
+	data () {
+		return {
+			brightness: 0,
+			sending: false
+		}
+	},
+	watch: {
+		brightness (newValue) {
+			console.log(`${this.lightId}: ${newValue}`)
+			this.sendBrightness()
+		}
+	},
+	created () {
+		this.sendBrightness = _.debounce(function () {
+			this.sending = true
+
+			var value = this.brightness
+			var id = this.lightId
+			console.log(`(${id}) Sending ${value}...`)
+			axios.put(
+				`${apiURL}/lights/${id}`,
+				{ value: value }
+			).then(resp => {
+				console.log(`(${id}) From server: ${resp.data.value}`)
+			}).finally(() => { this.sending = false })
+		}, this.debounceRate)
+	},
+	mounted () {
+		this.updateBrightness()
+		setInterval(() => this.updateBrightness(), this.updateRate)
+	},
+	methods: {
+		updateBrightness () {
+			if (this.sending) return // don't do anything if currently sending data
+
+			axios
+				.get(`${apiURL}/lights/${this.lightId}`)
+				.then(resp => {
+					console.log(`(${this.lightId}) From server: ${resp.data.value}`)
+					this.brightness = resp.data.value
+				})
+		}
+	}
+}
+</script>
--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,10 +1,28 @@
 <template>
-	<p>Hello world!</p>
+	<div>
+		<h1><logo /></h1>
+		<div>
+			<h2>Living Room</h2>
+			<light-control
+				name="North"
+				:light-id="0"
+			/>
+			<light-control
+				name="West"
+				:light-id="1"
+			/>
+		</div>
+	</div>
 </template>
 
 <script>
+import Logo from 'components/Logo'
+import LightControl from 'components/LightControl.vue'
+
 export default {
 	components: {
+		Logo,
+		LightControl
 	}
 }
 </script>
