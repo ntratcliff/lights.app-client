f55ff21f17222278459918e07dcde23d0f7c34a2
Noah Ratcliff <me@nratcliff.com>
2020-12-21 14:38:01 -0500

    feat: setup router

--- src/App.vue
+++ src/App.vue
@@ -2,18 +2,16 @@
 	<div>
 		<navbar />
 		<div class="container">
-			<hello-component name="World" />
+			<router-view />
 		</div>
 	</div>
 </template>
 
 <script>
-import HelloComponent from './components/HelloComponent.vue'
 import Navbar from './components/Navbar.vue'
 
 export default {
 	components: {
-		HelloComponent,
 		Navbar
 	}
 }
--- src/index.js
+++ src/index.js
@@ -1,12 +1,19 @@
 import Vue from 'vue'
+import VueRouter from 'vue-router'
 import BootstrapVue from 'bootstrap-vue'
 import App from './App.vue'
 
 import '../assets/app.scss'
+import routes from 'src/routes'
 
+Vue.use(VueRouter)
 Vue.use(BootstrapVue)
 
-export default new Vue({
-	el: '#app',
-	render: h => h(App)
+const router = new VueRouter({
+	routes
 })
+
+export default new Vue({
+	router: router,
+	render: h => h(App)
+}).$mount('#app')
